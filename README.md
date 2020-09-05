04afc530e3d18015ba80ac926c128efc2f956770
Dima Hinev <dh@MacBook-Pro.local>
2020-09-05 17:53:54 +0300

    added README

---
+++ README.md
@@ -0,0 +1,10 @@
+
+<h1 align="center">TimeTable ⏰</h1>
+<div align="middle">
+  <img src="./screen1.png" width='300px' />
+  <img src="./screen2.png" width='300px'/>
+</div>
+
+# Usage
+1. Install app on your computer
+2. Change timetable.txt, if you want (observing the design)
--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const timer = (
   para,
   time_text = `${COLOR.FORE_PURPLE_BOLD}time:${COLOR.RESET}`,
   para_text = `${COLOR.FORE_CYAN_BOLD}para:${COLOR.RESET}`,
-  shedule
+  timetable
 ) => {
   let count = 0
   return new Promise((resolve) => {
@@ -16,17 +16,17 @@ const timer = (
       date = parseInt(date.getTime(), 10) - count // translate time in number and subtract
       date = new Date(date) // get new date from previous result
 
-      let shedule_arr = shedule.split('\n')
+      let timetable_arr = timetable.split('\n')
 
       console.clear()
       
       let str = 
       `
-${(1 === para ? `${COLOR.FORE_RED_BOLD}1) ${shedule_arr[0]} < ${COLOR.RESET}`: `1) ${shedule_arr[0]}`)}
-${(2 === para ? `${COLOR.FORE_RED_BOLD}2) ${shedule_arr[1]} < ${COLOR.RESET}`: `2) ${shedule_arr[1]}`)}
-${(3 === para ? `${COLOR.FORE_RED_BOLD}3) ${shedule_arr[2]} < ${COLOR.RESET}`: `3) ${shedule_arr[2]}`)}
-${(4 === para ? `${COLOR.FORE_RED_BOLD}4) ${shedule_arr[3]} < ${COLOR.RESET}`: `4) ${shedule_arr[3]}`)}
-${(5 === para ? `${COLOR.FORE_RED_BOLD}5) ${shedule_arr[4]} < ${COLOR.RESET}`: `5) ${shedule_arr[4]}`)}
+${(1 === para ? `${COLOR.FORE_RED_BOLD}1) ${timetable_arr[0]} < ${COLOR.RESET}`: `1) ${timetable_arr[0]}`)}
+${(2 === para ? `${COLOR.FORE_RED_BOLD}2) ${timetable_arr[1]} < ${COLOR.RESET}`: `2) ${timetable_arr[1]}`)}
+${(3 === para ? `${COLOR.FORE_RED_BOLD}3) ${timetable_arr[2]} < ${COLOR.RESET}`: `3) ${timetable_arr[2]}`)}
+${(4 === para ? `${COLOR.FORE_RED_BOLD}4) ${timetable_arr[3]} < ${COLOR.RESET}`: `4) ${timetable_arr[3]}`)}
+${(5 === para ? `${COLOR.FORE_RED_BOLD}5) ${timetable_arr[4]} < ${COLOR.RESET}`: `5) ${timetable_arr[4]}`)}
 
 ${COLOR.FORE_RED_BOLD}${para}${COLOR.RESET} ${para_text} | ${time_text} ${COLOR.FORE_ORANGE_BOLD}${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}${COLOR.RESET}
 `
@@ -75,8 +75,8 @@ const between = (date, hmin, mmin, hmax, mmax) => {
 const app = async () => {
   // узнать какая сейчас пара
   let date = { hour: new Date().getHours(), minutes: new Date().getMinutes() }
-  // date.hour = 16
-  // date.minutes = 9 
+  date.hour = 9
+  date.minutes = 35
   let para = 1
 
   // Пары еще не начались (12:00 - 8:00)
@@ -93,26 +93,27 @@ const app = async () => {
     process.exit()
   }
 
-  // Прочитать shedule.txt в shedule
+  // Прочитать timetable.txt в timetable
   let fs = require('fs')
-  let shedule = fs.readFileSync('shedule.txt', 'utf8')
-  let cp_shedule = shedule.split('\n')
+  let timetable = fs.readFileSync('timetable.txt', 'utf8')
+  let cp_timetable = timetable.split('\n')
   let time_remaining = 0
 
   // Считать время перемены
   const peremena = (para) => {
-    let row1 = cp_shedule[para - 1]
-    let row2 = cp_shedule[para]  
+    let row1 = cp_timetable[para - 1]
+    let row2 = cp_timetable[para]  
     try {  
       // hmax = 9
-      let hmax1 = row1.split('-')[1].split(':')[0]
+      var hmax1 = row1.split('-')[1].split(':')[0]
       // mmax = 30
-      let mmax1 = row1.split('-')[1].split(':')[1]
+      var mmax1 = row1.split('-')[1].split(':')[1]
 
       // hmax = 9
-      let hmax2 = row2.split('-')[0].split(':')[0]
+      var hmax2 = row2.split('-')[0].split(':')[0]
       // mmax = 40
-      let mmax2 = row2.split('-')[0].split(':')[1]
+      var mmax2 = row2.split('-')[0].split(':')[1]
+
     } catch {
       // Пары закончились
       console.clear()
@@ -120,18 +121,18 @@ const app = async () => {
       process.exit()
     }
 
-    let date1 = new Date((hmax1 * 60 * 60 * 1000) + (mmax1 * 60 * 1000)).getTime()
-    let date2 = new Date((hmax2 * 60 * 60 * 1000) + (mmax2 * 60 * 1000)).getTime()
+    var date1 = new Date((hmax1 * 60 * 60 * 1000) + (mmax1 * 60 * 1000)).getTime()
+    var date2 = new Date((hmax2 * 60 * 60 * 1000) + (mmax2 * 60 * 1000)).getTime()
 
-    let diff = Math.abs(date2 - date1)
+    var diff = Math.abs(date2 - date1)
     diff = new Date(diff).getMinutes()
     return diff
   }
   
 
-  for (let i = 0; i < cp_shedule.length; i++) {
+  for (let i = 0; i < cp_timetable.length; i++) {
     // row = 8:00-9:30
-    let row = cp_shedule[i]
+    let row = cp_timetable[i]
     // hmin = 8
     let hmin = row.split('-')[0].split(':')[0]
     // mmin = 00
@@ -167,17 +168,17 @@ const app = async () => {
       // i + 1 т.к i начинается с нуля
       para = i + 1
 
-      await timer(time_remaining, para++, 'осталось', 'перемена', shedule)
+      await timer(time_remaining, para++, 'осталось', 'перемена', timetable)
       time_remaining = 90
       break
     }
   }
 
   while (para < 6) {
-    await timer(time_remaining, para, `осталось`, `пара`, shedule)
-    await timer(peremena(para), para++, 'осталось', 'перемена', shedule)
-    // await timer(0.05, para, `осталось`, `пара`, shedule)
-    // await timer(peremena(para), para++, 'осталось', 'перемена', shedule)
+    await timer(time_remaining, para, `осталось`, `пара`, timetable)
+    await timer(peremena(para), para++, 'осталось', 'перемена', timetable)
+    // await timer(0.05, para, `осталось`, `пара`, timetable)
+    // await timer(peremena(para), para++, 'осталось', 'перемена', timetable)
   }
   // Пары закончились
   console.clear()
